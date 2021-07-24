0da99deb0c908bea1fa8aa74719383b3f4312ebf
Vidya R <67091187+VidyaR007@users.noreply.github.com>
2021-07-24 21:28:54 +0530

    Add files via upload

---
+++ concatinate.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+   double a = 4.0;
+   double b = 4.0;
+   cin>>a>>b;
+   cout<<"\nHello"<<fixed<<setprecision(1)<<a+b;
+}
